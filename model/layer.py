a7d65e058351b0342b53338f794fd439a0cd6a2f
zhangmengling <47131841+zhangmengling@users.noreply.github.com>
2022-02-14 14:44:30 +0800

    Update layer.py

--- model/layer.py
+++ model/layer.py
@@ -1,6 +1,6 @@
 import numpy as np
-# import tensorflow as tf
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
+# import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 
 class Layer(object):
